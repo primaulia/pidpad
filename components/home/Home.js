e5778cf1e371b6704229c4c378d10262cc68bc05
Prima Aulia Gusta <primaulia@gmail.com>
2017-05-24 17:04:16 +0800

    setup

---
+++ components/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+class Home extends React.Component {
+
+  render () {
+    return (
+      <Text>Hello test!</Text>
+    )
+  }
+
+}
+
+export default Home
